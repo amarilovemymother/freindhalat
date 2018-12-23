ac90255a7e05690bbe571e8b17879518a1d8172e
amarilovemymother <46085621+amarilovemymother@users.noreply.github.com>
2018-12-23 15:39:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "T";
 var adminprefix = 'T'
 
-const developers = ["477591209943105538"]
+const developers = ["515236282687815743"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
